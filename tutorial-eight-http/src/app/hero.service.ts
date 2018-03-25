77e2435ea10195386954b6adada98ee9d077357d
himu62 <hiromu_kobayashi@voyagegroup.com>
2018-03-25 13:40:32 +0900

    deleteHero api

--- tutorial-eight-http/src/app/hero.service.ts
+++ tutorial-eight-http/src/app/hero.service.ts
@@ -49,6 +49,15 @@ export class HeroService {
     );
   }
 
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+
+    return this.http.delete<Hero>(`${this.heroesUrl}/${id}`, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
